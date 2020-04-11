fb05e3ef4b7216fc20f46cfb3a086bfbcdbce255
Pierre Guillen <hi@cerbero.dev>
2020-04-11 17:07:30 -0500

    Update 7 | Var - Interface

---
+++ TypeScript/Example1/interfaces.ts
@@ -0,0 +1,15 @@
+interface Article {
+    title: string,
+    subtitle: string,
+    body: string,
+    author: string
+}
+
+var post1: Article = {
+    title: "Aprendiendo TypeScript",
+    subtitle: "Angular con TypeScript",
+    body: "Qui qui culpa irure est ex nisi fugiat duis eiusmod velit. Do nostrud id non proident et. Aliqua nulla anim pariatur sit aliqua fugiat in esse minim tempor aliqua Lorem. Eu nulla Lorem qui veniam dolore ea eiusmod ad veniam eu. Fugiat nisi est sunt tempor qui ad consequat aliquip elit. Cupidatat officia adipisicing exercitation enim consequat irure.",
+    author: "Pierre Guillen"
+}
+
+console.log(post1)
