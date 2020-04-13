5197b92fbf491200f04b03f19531e2d811969e95
Pierre Guillen <hi@cerbero.dev>
2020-04-12 20:25:17 -0500

    Update 14 | Conditionals - TERNARIOS

---
+++ TypeScript/Conditional/ternarios.js
@@ -0,0 +1,9 @@
+var nam3 = 'Pierre';
+var age = 9;
+var evolution;
+if (age < 10) {
+    evolution = ' is kid';
+}
+console.log(evolution);
+evolution = age > 10 ? "No is Kid" : evolution;
+console.log(evolution);
