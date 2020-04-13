4e4b7c7d8ce520d3167bb4fda529928e621af0fd
Pierre Guillen <hi@cerbero.dev>
2020-04-12 20:15:34 -0500

    Update 14 | Conditionals - SWITCH

---
+++ TypeScript/Conditional/switch.js
@@ -0,0 +1,18 @@
+var ticketSold = 12500;
+switch (ticketSold) {
+    case 12500: {
+        console.log("Your number of ticket is: " + ticketSold);
+        break;
+    }
+    case 20000: {
+        console.log("Your number of ticket is: " + ticketSold);
+        break;
+    }
+    case 20500: {
+        console.log("Your number of ticket is: " + ticketSold);
+        break;
+    }
+    default: {
+        console.log("Your number of ticket not found");
+    }
+}
---
+++ TypeScript/Conditional/switch.ts
@@ -0,0 +1,21 @@
+var ticketSold: number = 12500
+
+switch (ticketSold) {
+    case 12500: {
+        console.log("Your number of ticket is: " + ticketSold)
+        break
+    }
+    case 20000: {
+        console.log("Your number of ticket is: " + ticketSold)
+        break
+    }
+    case 20500: {
+        console.log("Your number of ticket is: " + ticketSold)
+        break
+    }
+    default: {
+        console.log("Your number of ticket not found")
+
+    }
+}
+
